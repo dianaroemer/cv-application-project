43321a098eeb753e7b25268fba971327566d84a5
dominicroemer <dominicroemer@gmail.com>
2022-07-08 22:53:40 -0700

    Integrate previous portfolio styling for navBar into dedicated component for easy reuse

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Styling/NavbarStyle.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends Component {
     // constructor(props) {
@@ -9,9 +11,39 @@ class Navbar extends Component {
     render() {
 
         return (
-            <div className='navbarContainer' >
-                <h3>I am Navbar.js. Build me.</h3>
-            </div>
+            <nav className='navbarContainer' >
+                
+                <div className='navLeft'>
+                    D Roemer             
+                    {/* <img src="images/droemer.png" class="nav-droemer">  */}
+                    {/* <FontAwesomeIcon icon={faUser} onClick={e=> preventDefault()}/> */}
+                </div>
+                
+                <div className="navTitle">
+                    CV-Application Project
+                </div>
+                
+
+
+                <ul>
+                    <li className="navOptionsToggle">Options</li>
+                    <li className="navAboutToggle">About</li>
+                    <li className="navPortfolioToggle">
+                        <a href="https://github.com/dominicroemer">Portfolio</a>
+                    </li>
+                </ul>
+
+                {/* <img src="images/nav-menu.png" class="nav-menu-icon"> */}
+
+                <FontAwesomeIcon icon={faBars} 
+                    className="navMenuIcon"
+                    onClick={e=> e.preventDefault()}/>
+                <FontAwesomeIcon icon={faUser} 
+                    className='navMenuIcon'
+                    onClick={e=> e.preventDefault()}/>
+
+
+            </nav>
         )
     }
 }

296b1019133b00c744f964290bf9121cbc3fac6f
dominicroemer <dominicroemer@gmail.com>
2022-07-08 23:28:35 -0700

    Style sample CV button

--- src/Components/FormContainer.js
+++ src/Components/FormContainer.js
@@ -16,8 +16,8 @@ class FormContainer extends Component {
 
 
         return (
-            <div className='FormContainer' style={{borderStyle: 'solid', borderRadius: 2, borderColor: 'green', borderWidth: 10}}>
-                <h3>I am FormContainer.js. I contain forms. Let's put something together</h3>
+            <div className='FormContainer'>
+                {/* <h3>I am FormContainer.js. I contain forms. Let's put something together</h3> */}
                 <button type="submit" onClick={setSampleState} className='sampleStateButton'>Generate Sample CV</button>
                 <PersonalInformationForm 
                     updateInputField={updateInputField}
